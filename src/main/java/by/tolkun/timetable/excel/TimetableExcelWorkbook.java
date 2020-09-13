64cbf99be7e4c30736be23b4b5e2ce4adb9b0ff8
kirill <tolkun_14@inbox.ru>
2020-09-13 15:19:26 +0300

    Add class 'TimetableExcelWorkbook'

---
+++ src/main/java/by/tolkun/timetable/excel/TimetableExcelWorkbook.java
@@ -0,0 +1,41 @@
+package by.tolkun.timetable.excel;
+
+import org.apache.poi.ss.usermodel.Workbook;
+
+import java.util.Objects;
+
+public class TimetableExcelWorkbook {
+    private Workbook workbook;
+
+    public TimetableExcelWorkbook(Workbook workbook) {
+        this.workbook = workbook;
+    }
+
+    public Workbook getWorkbook() {
+        return workbook;
+    }
+
+    public void setWorkbook(Workbook workbook) {
+        this.workbook = workbook;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof TimetableExcelWorkbook)) return false;
+        TimetableExcelWorkbook that = (TimetableExcelWorkbook) o;
+        return workbook.equals(that.workbook);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(workbook);
+    }
+
+    @Override
+    public String toString() {
+        return "TimetableExcelWorkbook{" +
+                "workbook=" + workbook +
+                '}';
+    }
+}
