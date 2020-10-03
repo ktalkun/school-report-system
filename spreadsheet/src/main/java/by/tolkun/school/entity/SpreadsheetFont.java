5e7e1f1e8eba004c21e59db7b97820b94407cacd
kirill <tolkun_14@inbox.ru>
2020-10-03 15:09:49 +0300

    Add class 'SpreadsheetFont'. Add necessary fields

---
+++ spreadsheet/src/main/java/by/tolkun/school/entity/SpreadsheetFont.java
@@ -0,0 +1,12 @@
+package by.tolkun.school.entity;
+
+public class SpreadsheetFont {
+    private String fontName;
+    private Integer fontOffset;
+    private Boolean isBold;
+    private Boolean isItalic;
+    private Boolean isUnderlined;
+    private Boolean isDoubleUnderlined;
+    private Boolean isStrikeout;
+    private Integer sizeInPoints;
+}
