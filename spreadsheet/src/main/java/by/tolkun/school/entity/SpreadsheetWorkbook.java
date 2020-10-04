db0e54ed5a870c5a4eb3b132926395b2ebda7cfc
kirill <tolkun_14@inbox.ru>
2020-10-05 00:31:37 +0300

    Add maps 'tabsByIndex' and 'tabsByTitle'. Add methods 'getTab()' to get tab by title of by index

--- spreadsheet/src/main/java/by/tolkun/school/entity/SpreadsheetWorkbook.java
+++ spreadsheet/src/main/java/by/tolkun/school/entity/SpreadsheetWorkbook.java
@@ -2,6 +2,9 @@ package by.tolkun.school.entity;
 
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Class to represent workbook.
  */
@@ -11,6 +14,16 @@ public class SpreadsheetWorkbook {
      * Poi workbook.
      */
     private XSSFWorkbook workbook;
+
+    /**
+     * Map of tabs by index.
+     */
+    private final Map<Integer, SpreadsheetTab> tabsByIndex = new HashMap<>();
+
+    /**
+     * Map of tabs by title.
+     */
+    private final Map<String, SpreadsheetTab> tabsByTitle = new HashMap<>();
 
     /**
      * Default constructor.
@@ -26,6 +39,26 @@ public class SpreadsheetWorkbook {
      */
     public SpreadsheetWorkbook(XSSFWorkbook workbook) {
         this.workbook = workbook;
+    }
+
+    /**
+     * Get tab by index.
+     *
+     * @param index the index of tab (sheet)
+     * @return tab (sheet)
+     */
+    public SpreadsheetTab getTab(int index) {
+        return tabsByIndex.get(index);
+    }
+
+    /**
+     * Get tab by title.
+     *
+     * @param title the title of tab (sheet)
+     * @return tab (sheet)
+     */
+    public SpreadsheetTab getTab(String title) {
+        return tabsByTitle.get(title);
     }
 
     /**
