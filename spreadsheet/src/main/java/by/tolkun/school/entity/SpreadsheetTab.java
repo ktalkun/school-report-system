ee5e18ff5e1da2d42833e3ad0ae5e3f1973f0e52
kirill <tolkun_14@inbox.ru>
2020-10-05 00:00:38 +0300

    Add class 'SpreadsheetTab' with necessary fields and constructors

---
+++ spreadsheet/src/main/java/by/tolkun/school/entity/SpreadsheetTab.java
@@ -0,0 +1,41 @@
+package by.tolkun.school.entity;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+
+/**
+ * Class to represent tab (sheet) of workbook.
+ */
+public class SpreadsheetTab {
+
+    /**
+     * Workbook of excel document.
+     */
+    private final SpreadsheetWorkbook spreadsheetWorkbook;
+
+    /**
+     * Sheet (tab) of workbook.
+     */
+    private final XSSFSheet sheet;
+
+    /**
+     * Constructor with parameters.
+     *
+     * @param spreadsheetWorkbook the workbook
+     * @param title    the title of new sheet
+     */
+    SpreadsheetTab(SpreadsheetWorkbook spreadsheetWorkbook, String title) {
+        this.spreadsheetWorkbook = spreadsheetWorkbook;
+        this.sheet = spreadsheetWorkbook.getPoiWorkbook().createSheet(title);
+    }
+
+    /**
+     * Constructor with parameters.
+     *
+     * @param spreadsheetWorkbook the workbook
+     * @param sheet    the sheet
+     */
+    SpreadsheetTab(SpreadsheetWorkbook spreadsheetWorkbook, XSSFSheet sheet) {
+        this.spreadsheetWorkbook = spreadsheetWorkbook;
+        this.sheet = sheet;
+    }
+}
