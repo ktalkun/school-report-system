4fcc6da888067f518d4cd65be19721bd62ff2ed9
Kirill Tolkun <tolkun_14@inbox.ru>
2020-10-09 22:31:40 +0300

    Create class 'StudentTimetableParser' to parse tab into student timetable

---
+++ timetable/src/main/java/by/tolkun/school/parser/StudentTimetableParser.java
@@ -0,0 +1,8 @@
+package by.tolkun.school.parser;
+
+/**
+ * Class to parse tab {@link by.tolkun.school.entity.SpreadsheetTab} into
+ * student timetable {@link by.tolkun.school.entity.StudentTimetable}.
+ */
+public class StudentTimetableParser {
+}
